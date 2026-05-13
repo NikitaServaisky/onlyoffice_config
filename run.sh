879e1512f04830e9ea92992aa590057e65b0e37d
Nikita <servaiski@gmail.com>
2026-05-13 20:00:54 +0300

    Initial commit

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Update stockpile of fonts
+fc-cache -fv
+
+# Run script for scan fo fonts
+/usr/bin/documentserver-generate-allfonts.sh
+
+# Continue to running Entrypoint of Onlyoffice
+/user/local/bin/enterypoint.sh
