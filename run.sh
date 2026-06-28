843685cdfa99123a16d37d839923c078299c35e4
Nikita <servaiski@gmail.com>
2026-06-28 22:34:37 +0300

    docker vs me round 5

--- run.sh
+++ run.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
 # Update stockpile of fonts
 fc-cache -fv
-
-# Run script for scan of fonts
 /usr/bin/documentserver-generate-allfonts.sh
 
-# Continue to running Entrypoint of Onlyoffice
 exec /usr/bin/documentserver-entrypoint.sh
